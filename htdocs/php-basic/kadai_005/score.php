c297ecfc30158e7917be02e24de6d20070f6355a
yamazaki88 <yknmkn@yahoo.co.jp>
2024-10-21 21:32:19 +0900

    kadai_005
    kadai_005を提出

---
+++ htdocs/php-basic/kadai_005/score.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>テストの平均点を計算しよう</title>
+</head>
+
+<body>
+  <p>
+<?php
+$user_score = 80+60+55+40+100+25+80+95+30+60;
+echo $user_score / 10;
+?>
+</p>
+</body>
+</html>
