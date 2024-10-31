2b7877a8a7c69bccc622ac3dcbc50d3047783a11
yamazaki88 <yknmkn@yahoo.co.jp>
2024-10-31 23:33:51 +0900

    sort.phpを再提出

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -8,31 +8,22 @@
 
 <body>
     <p>
-        <?php
-        function sort_2way($array, $order)
-        {
-            if ($order == "asc") {
-                sort($array);
-            } else if ($order == "desc") {
-                rsort($array);
+        <?php 
+        $nums = array(15, 4, 18, 23, 10);
+        function sort_2way($array, $order){
+            if ($order) {
+              echo "昇順にソートします。<br>";   
+              sort($array); 
+            }else{
+              echo "降順にソートします。<br>";
+              rsort($array);
             }
-            return $array;
-        }
-        $nums = [15, 4, 18, 23, 10];
-
-        // 昇順にソート
-        echo "昇順にソートします。<br>";
-        $sorted_nums_asc = sort_2way($nums, "asc");
-        foreach ($sorted_nums_asc as $num) {
-            echo $num . "<br>";
-        }
-        
-        // 降順にソート
-        echo "降順にソートします。<br>";
-        $sorted_nums_desc = sort_2way($nums, "desc");
-        foreach ($sorted_nums_desc as $num) {
-            echo $num . "<br>";
-        }
+              foreach($array as $element){
+                echo $element.'<br>';
+              }
+            }
+                sort_2way($nums,true);
+                sort_2way($nums,false);
         ?>
 </body>
 
